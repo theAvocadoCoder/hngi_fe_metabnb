b6159d4f5554fb0177aacdb76dbafc4a78e1f610
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-15 11:21:33 +0100

    chore: Create data module for bnb locations

---
+++ src/data_modules/locations.js
@@ -0,0 +1,140 @@
+const locations = {
+  layer1: [
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 1/dvn 1.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 1/s5.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 1/s11.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 1/S16.png"
+    },
+  ],
+  layer2: [
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 2/aise 1.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 2/aise 2.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 2/druids 6.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 2/s1.png"
+    },
+  ],
+  layer3: [
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 3/druids 4.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 3/s2.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 3/s8.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 3/s12.png"
+    },
+  ],
+  layer4: [
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 4/s9.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 4/susan 3.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 4/susan 5.png"
+    },
+    {
+      name: "Desert king",
+      price: "1MBT per night",
+      distance: "2345km",
+      duration: "2weeks",
+      stars: 5,
+      imgUrl: "../images/locations/layer 4/susan8.png"
+    },
+  ],
+}
+
+export default locations;
