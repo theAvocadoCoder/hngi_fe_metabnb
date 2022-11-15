cd929360314a13e7f6f1abec842e13e3ea478d74
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-15 19:22:58 +0100

    chore: Implement styled featured places section

--- src/routes/App.js
+++ src/routes/App.js
@@ -7,8 +7,12 @@ import susan1 from "../images/locations/susan 1.png";
 import mbToken from "../images/svg/mbtoken.svg";
 import metaMask from "../images/svg/metamask.svg";
 import openSea from "../images/svg/opensea.svg";
+import locations from '../data_modules/locations';
+import LocationCard from '../components/LocationCard';
 
 function App() {
+  const { layer1, layer2 } = locations;
+
   return (
     <div className="App">
       <section className='section__hero'>
@@ -36,9 +40,26 @@ function App() {
           </div>
         </div>
         <div className='hero-div__platforms'>
-          <img src={mbToken} alt="" />
-          <img src={metaMask} alt="" />
-          <img src={openSea} alt="" />
+          <img src={mbToken} alt="MBToken logo" />
+          <img src={metaMask} alt="MetaMask logo" />
+          <img src={openSea} alt="OpenSea logo" />
+        </div>
+      </section>
+      <section className='section__featured-places'>
+        <h1>Inspiration for your next adventure</h1>
+        <div className='featured-places__div'>
+          {
+            layer1.map((layer1Location, index)=>(
+              <LocationCard bnbLocation={layer1Location} key={index} />
+            ))
+          }
+        </div>
+        <div className='featured-places__div'>
+          {
+            layer2.map((layer2Location, index)=>(
+              <LocationCard bnbLocation={layer2Location} key={index} />
+            ))
+          }
         </div>
       </section>
     </div>
